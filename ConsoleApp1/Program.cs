4ab5ec3754b4d2a92f7248ebba78652a40900d44
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-11-27 19:00:31 -0300

    Created VS project, completed task A

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static int Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        int a;
+        bool rez = int.TryParse(s, out a);
+        Console.WriteLine("Square's Perimeter: {0}", a * 4);
+        Console.WriteLine("Square's Area: {0}", a * a);
+        return 0;
+    }
+}
