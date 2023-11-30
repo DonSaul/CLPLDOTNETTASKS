dfd33af8fab3b6dd3b41d75bd27273904bd8b74d
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-11-30 18:12:44 -0300

    Refactorized first part of task 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,26 +2,31 @@
 {
     public static void Main(string[] args)
     {
+        DayMonthTest();
+    }
+
+    private static void DayMonthTest()
+    {
         Console.WriteLine("Enter 2 integers to check if they can be day and month:");
         int a, b;
         Console.Write("a: ");
         string s = Console.ReadLine();
         bool rez = Int32.TryParse(s, out a);
-        if (rez )
+        if (!rez )
         {
-            Console.Write("b: ");
-            s = Console.ReadLine();
-            rez = Int32.TryParse(s, out b);
-            if (rez )
-            {
-                bool result = IsDayAndMonthPair(a, b);
-                Console.WriteLine("{0} a day and month pair!", result ? "Yes, it is" : "No, it's not");
-            } else {
-                Console.WriteLine("You didn't type a number!!!");
-            }
-        } else {
             Console.WriteLine("You didn't type a number!!!");
+            return;
         }
+        Console.Write("b: ");
+        s = Console.ReadLine();
+        rez = Int32.TryParse(s, out b);
+        if (!rez )
+        {
+            Console.WriteLine("You didn't type a number!!!");
+            return;
+        }
+        bool result = IsDayAndMonthPair(a, b);
+        Console.WriteLine("{0} a day and month pair!", result ? "Yes, it is" : "No, it's not");
     }
 
     private static bool IsDayAndMonthPair(int a, int b)
