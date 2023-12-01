ed891422381bb51687729a906fc50a71d25f3c1d
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-01 18:40:17 -0300

    Completed first task of H2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,7 +1,28 @@
-﻿public class Program2
+﻿
+public class Program2
 {
     public static void Main(string[] args)
     {
-        
+        CheckForFloatsInRange();
+    }
+
+    private static void CheckForFloatsInRange()
+    {
+        float[] floats = new float[3];
+        for (int i = 0; i < floats.Length; i++)
+        {
+            string? s = Console.ReadLine();
+            bool rez = float.TryParse(s, out float value);
+            if (rez) { floats[i] = value; } else
+            {
+                Console.WriteLine("Sorry, that's not even a number!");
+                return;
+            }
+        }
+        foreach (float fl in floats)
+        {
+            string result = (fl >= -5 && fl <= 5) ? "INDEED" : "NOT";
+            Console.WriteLine("{0:F2} is {1} within the range [-5, 5]", fl, result);
+        }
     }
 }
