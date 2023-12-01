0e4635a58887b5ccc0719314bc3018760db79df7
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-01 19:11:47 -0300

    Completed task 2 of H2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,9 +1,13 @@
 ﻿
+
 public class Program2
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("1) Floats in range");
         CheckForFloatsInRange();
+        Console.WriteLine("2) Min/max of 3 ints");
+        CheckMaxMinInts();
     }
 
     private static void CheckForFloatsInRange()
@@ -12,7 +16,7 @@ public class Program2
         float[] floats = new float[3];
         for (int i = 0; i < floats.Length; i++)
         {
-            Console.Write("{0}>", i);
+            Console.Write("{0}>", (i + 1));
             string? s = Console.ReadLine();
             bool rez = float.TryParse(s, out float value);
             if (rez) { floats[i] = value; } else
@@ -26,5 +30,30 @@ public class Program2
             string result = (fl >= -5 && fl <= 5) ? "INDEED" : "NOT";
             Console.WriteLine("{0:F2} is {1} within the range [-5, 5]", fl, result);
         }
+    }
+
+    private static void CheckMaxMinInts()
+    {
+        Console.WriteLine("Type in 3 entegers, please:");
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        for(int i = 0;i < 3; i++)
+        {
+            Console.Write("{0}>", (i + 1));
+            string? s = Console.ReadLine();
+            bool rez = Int32.TryParse(s, out int value);
+            if (rez)
+            {
+                max = int.Max(max, value);
+                min = int.Min(min, value);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that's not an integer!");
+                return;
+            }
+        }
+        Console.WriteLine("The max of those ints is {0}", max);
+        Console.WriteLine("The min of those ints is {0}", min);
     }
 }
