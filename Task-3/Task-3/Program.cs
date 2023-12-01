5878eae84c470289d956faa714afe635eded7b1a
rainclaudio <rainclaudio25@gmai.com>
2023-12-01 14:42:13 -0300

    user usability done

--- Task-3/Task-3/Program.cs
+++ Task-3/Task-3/Program.cs
@@ -3,29 +3,29 @@ using Task_3.src;
 
 class Program
 {
-    static void Main(string[] args) // Ensure 'Main' is capitalized
+    static void Main(string[] args)
     {
         var userInteraction = new UserInteraction();
 
-        Console.WriteLine("Task #1:");
+        Console.WriteLine("Task 1: Count Integers Divisible by 3 in a Range");
         userInteraction.HandleDivisibleByThree();
 
-        Console.WriteLine("\nTask #2:");
+        Console.WriteLine("\nTask 2: Print Every Second Character in a String");
         userInteraction.HandlePrintEverySecondChar();
 
-        Console.WriteLine("\nTask #3:");
+        Console.WriteLine("\nTask 3: Get Drink Price");
         userInteraction.HandleGetDrinkPrice();
 
-        Console.WriteLine("\nTask #4:");
+        Console.WriteLine("\nTask 4: Calculate Arithmetic Average of Positive Integers");
         userInteraction.HandleCalculateAverage();
 
-        Console.WriteLine("\nTask #5:");
+        Console.WriteLine("\nTask 5: Check if a Year is a Leap Year");
         userInteraction.HandleIsLeapYear();
 
-        Console.WriteLine("\nTask #6:");
+        Console.WriteLine("\nTask 6: Find Sum of Digits of an Integer");
         userInteraction.HandleSumOfDigits();
 
-        Console.WriteLine("\nTask #7:");
+        Console.WriteLine("\nTask 7: Check if Integer Contains Only Odd Numbers");
         userInteraction.HandleHasOnlyOddDigits();
 
         Console.WriteLine("\nMany thanks for trying this program! Have a good day! Process Terminated.");
