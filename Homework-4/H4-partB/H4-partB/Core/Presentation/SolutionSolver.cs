f702a76c6b47a932bd6fdc50820afd8935acfba5
rainclaudio <rainclaudio25@gmai.com>
2023-12-06 19:47:11 -0300

    Homework 4 part B finished

---
+++ Homework-4/H4-partB/H4-partB/Core/Presentation/SolutionSolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Homework_4.src.Core.Entities;
+
+namespace Homework_4.src.Core.Presentation
+{
+	public class SolutionSolver
+	{
+        public void WriteShapesWithAreaInRangeToFile(List<Shape> shapes, double minArea, double maxArea, string fileName)
+        {
+            var filteredShapes = shapes.Where(s => s.Area() >= minArea && s.Area() <= maxArea).ToList();
+            File.WriteAllLines(fileName, filteredShapes.Select(s => s.ToString()));
+        }
+
+        public void WriteShapesWithNameContainingToFile(List<Shape> shapes, string character, string fileName)
+        {
+            var filteredShapes = shapes.Where(s => s.Name.Contains(character)).ToList();
+            File.WriteAllLines(fileName, filteredShapes.Select(s => s.ToString()));
+        }
+
+        public void RemoveShapesWithPerimeterLessThan(List<Shape> shapes, double minPerimeter)
+        {
+            shapes.RemoveAll(s => s.Perimeter() < minPerimeter);
+        }
+    }
+}
+
--- Homework-4/H4-partB/H4-partB/Program.cs
+++ Homework-4/H4-partB/H4-partB/Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
 
+class Program
+{
+    static void Main()
+    {
+        string filePath = "textfile.txt"; // Replace with your file path
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            string longestLine = "";
+            string shortestLine = lines.Length > 0 ? lines[0] : "";
+            int maxLineLength = 50; // Maximum length for displayed lines
+
+            Console.WriteLine("Line Analysis:");
+            Console.WriteLine(new string('-', 60));
+            Console.WriteLine($"{"Line",-50} | {"Symbols",7}");
+            Console.WriteLine(new string('-', 60));
+
+            foreach (string line in lines)
+            {
+                string truncatedLine = line.Length > maxLineLength
+                                        ? line.Substring(0, maxLineLength - 3) + "..."
+                                        : line;
+
+                Console.WriteLine($"{truncatedLine,-50} | {line.Length,7}");
+
+                if (line.Length > longestLine.Length) longestLine = line;
+                if (line.Length < shortestLine.Length) shortestLine = line;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Longest Line:");
+            Console.WriteLine(longestLine.Length > maxLineLength ? longestLine.Substring(0, maxLineLength) + "..." : longestLine);
+            Console.WriteLine();
+
+            Console.WriteLine("Shortest Line:");
+            Console.WriteLine(shortestLine);
+            Console.WriteLine();
+
+            Console.WriteLine("Lines Containing 'var':");
+            foreach (string line in lines.Where(l => l.Contains("var")))
+            {
+                string truncatedLine = line.Length > maxLineLength
+                                        ? line.Substring(0, maxLineLength - 3) + "..."
+                                        : line;
+                Console.WriteLine($"- {truncatedLine}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
