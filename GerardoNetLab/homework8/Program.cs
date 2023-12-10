7dde93ad2d8518ab6f5eab0c19f52ae356e90fce
Gerardo Rojas <rojasgerardo786@gmail.com>
2023-12-10 18:40:34 -0300

    HomerWork 8 (5 por órden) añadida

---
+++ GerardoNetLab/homework8/Program.cs
@@ -0,0 +1,58 @@
+﻿using netLabHW8;
+
+List<Shape> shapes = new List<Shape>();
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Ingresa los detalles para la figura {i + 1}:");
+    Console.Write("Nombre: ");
+    string name = Console.ReadLine();
+    Console.Write("Tipos (1 Círculo, 2 Cuadrado): ");
+    int type = int.Parse(Console.ReadLine());
+
+    if (type == 1)
+    {
+        Console.Write("Radio: ");
+        double radius = double.Parse(Console.ReadLine());
+        shapes.Add(new Circle(name, radius));
+    }
+    else if (type == 2)
+    {
+        Console.Write("Lados: ");
+        double side = double.Parse(Console.ReadLine());
+        shapes.Add(new Square(name, side));
+    }
+    else
+    {
+        Console.WriteLine("Tipo inválido, ingresa 1 para círculo y 2 para cuadrado");
+        i--;
+    }
+}
+
+Console.WriteLine("\nInformación de figuras:");
+
+foreach (Shape shape in shapes)
+{
+    Console.WriteLine($"Nombre: {shape.Name}, Área: {shape.Area()}, Perímetro: {shape.Perimeter()}");
+}
+
+Shape maxPerimeterShape = shapes[0];
+
+for (int i = 1; i < shapes.Count; i++)
+{
+    if (shapes[i].Perimeter() > maxPerimeterShape.Perimeter())
+    {
+        maxPerimeterShape = shapes[i];
+    }
+}
+
+Console.WriteLine($"\nFigura con mayor perímetro: {maxPerimeterShape.Name}");
+
+shapes.Sort();
+
+Console.WriteLine("\nFiguras ordenadas por área:");
+
+foreach (Shape shape in shapes)
+{
+    Console.WriteLine($"Nombre: {shape.Name}, Área: {shape.Area()}, Perímetro: {shape.Perimeter()}");
+}
---
+++ GerardoNetLab/homework8/Shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netLabHW8
+{
+    public abstract class Shape:IComparable<Shape>
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+        public int CompareTo(Shape other)
+        {
+            if (other == null) return 1;
+            return this.Area().CompareTo(other.Area());
+        }
+
+    }
+}
+
