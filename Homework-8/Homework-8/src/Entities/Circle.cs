76d4946df83741c01fc8108109bc354d06d5341e
rainclaudio <rainclaudio25@gmai.com>
2023-12-11 10:48:16 -0300

    Adding classes

---
+++ Homework-8/Homework-8/src/Entities/Circle.cs
@@ -0,0 +1,41 @@
+﻿public class Circle : Shape
+{
+    // Field
+    private double radius;
+
+    // Property
+    public double Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+
+    // Constructor with name
+    public Circle(string name, double radius) : base(name)
+    {
+        Radius = radius;
+    }
+
+    // Constructor without name (name is optional)
+    public Circle(double radius) : base("Circle")
+    {
+        Radius = radius;
+    }
+
+    public override string ToString()
+    {
+        return $"Circle (Name: {Name}, Radius: {Radius.ToString("F2")}, Area: {Area().ToString("F2")}, Perimeter: {Perimeter().ToString("F2")})";
+    }
+    // Overridden Area method
+    public override double Area()
+    {
+        return Math.Round(Math.PI * Radius * Radius, 2);
+    }
+
+    public override double Perimeter()
+    {
+        return Math.Round(2 * Math.PI * Radius, 2);
+    }
+}
+
+
---
+++ Homework-8/Homework-8/src/Entities/Shape.cs
@@ -0,0 +1,20 @@
+﻿public abstract class Shape
+{
+    public string Name { get; protected set; }
+
+    protected Shape(string name)
+    {
+        Name = name;
+    }
+
+
+    public abstract double Area();
+    public abstract double Perimeter();
+
+    public override string ToString()
+    {
+        return $"Shape: {Name}";
+    }
+
+}
+
