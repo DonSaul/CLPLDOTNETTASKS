7337180d4cc61c60fb9f292cbe001d60a98a3fdf
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-12 19:43:57 -0300

    Created person class for Task 8

---
+++ Task8/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Task8
+{
+    public class Person
+    {
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        virtual public string Name { get { return name; } }
+        virtual public void Print()
+        {
+            Console.WriteLine("Name: {0}", this.name);
+        }
+    }
+}
