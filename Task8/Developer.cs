f685eced99098ad4132e06887e36c4a6a5eb9518
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-13 12:38:13 -0300

    Created class Developer for task 8

---
+++ Task8/Developer.cs
@@ -0,0 +1,16 @@
+﻿namespace Task8
+{
+    public class Developer: Staff
+    {
+        private string level;
+        public Developer(string name, int salary, string level): base(name, salary)
+        {
+            this.level = level;
+        }
+        public override string Name { get { return string.Format("{0} {1} developer", base.Name, this.level); } }
+        public override void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
