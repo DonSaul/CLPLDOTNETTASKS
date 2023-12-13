0fd1e3a365e9ac87ad77d9b5b45f8e84da881fc3
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-12 21:05:14 -0300

    Created Staff class for Task 8

---
+++ Task8/Staff.cs
@@ -0,0 +1,16 @@
+﻿namespace Task8
+{
+    public class Staff: Person
+    {
+        private int salary;
+        public Staff(string name, int salary): base(name)
+        {
+            this.salary = salary;
+        }
+        public override string Name { get { return base.Name + " Staff"; } }
+        public override void Print()
+        {
+            Console.WriteLine("Person {0} has salary: ${1}", Name, this.salary);
+        }
+    }
+}
