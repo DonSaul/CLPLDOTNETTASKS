9e46c29714d61f408959142ffd82039ef4764590
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-13 12:13:15 -0300

    Created class Teacher for Task 8

---
+++ Task8/Teacher.cs
@@ -0,0 +1,16 @@
+﻿namespace Task8
+{
+    public class Teacher: Staff
+    {
+        private readonly string subject;
+        public Teacher(string name, int salary, string subject): base(name, salary)
+        {
+            this.subject = subject;
+        }
+        public override string Name {  get { return base.Name + " as Teacher"; } }
+        public override void Print()
+        {
+            Console.WriteLine("{0} on {1}", Name, this.subject);
+        }
+    }
+}
