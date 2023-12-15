5c8ce68adaab032a94a4863a5afcd5a82cc2528e
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-15 19:19:48 -0300

    Created abstract class Shape

---
+++ HW_8_9/Shape.cs
@@ -0,0 +1,11 @@
+﻿namespace HW_8_9
+{
+    // Trying out Primary Constructor
+    public abstract class Shape(string name)
+    {
+        private string name = name;
+        public string Name { get { return name; } }
+        public abstract double Area();
+        public abstract double Perimeter();
+    }
+}
