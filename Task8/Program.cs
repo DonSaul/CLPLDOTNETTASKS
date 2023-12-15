a1acbd37b86259e5e0224e2ecab23c551ddf076e
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-15 15:53:37 -0300

    Completed point 5 of Task 8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -23,7 +23,7 @@ class Program
         Console.WriteLine("Now, please type in the name of a person to see if it's in the list:");
         Console.Write("> ");
         string s = Console.ReadLine() ?? "";
-        
+
         bool found = false;
         foreach (Person person in people)
         {
@@ -42,5 +42,22 @@ class Program
         {
             Console.WriteLine("Ehhh... That person is not on the list.");
         }
+        Console.WriteLine("Now sorting the list by name...");
+        people.Sort(ComparePeopleByName);
+        using (StreamWriter writer = new("people.txt", false))
+        {
+            foreach (Person person in people)
+            {
+                person.Print();
+                writer.WriteLine(person.Name);
+            }
+        }
+        Console.WriteLine("List written to file: people.txt");
+
+    }
+
+    private static int ComparePeopleByName(Person person1, Person person2)
+    {
+        return person1.Name.CompareTo(person2.Name);
     }
 }
