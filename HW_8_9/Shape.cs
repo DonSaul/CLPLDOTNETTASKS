e7cca56c439fe31b19cc09307a7e0502a9f6d346
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-18 11:07:45 -0300

    shape name is read only

--- HW_8_9/Shape.cs
+++ HW_8_9/Shape.cs
@@ -3,7 +3,7 @@
     // Trying out Primary Constructor
     public abstract class Shape(string name)
     {
-        private string name = name;
+        private readonly string name = name;
         public string Name { get { return name; } }
         public abstract double Area();
         public abstract double Perimeter();
