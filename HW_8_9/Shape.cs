db027b0b378e11e3292891fe25aad7c2ba5901a9
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-18 17:09:07 -0300

    Implemented IComparable

--- HW_8_9/Shape.cs
+++ HW_8_9/Shape.cs
@@ -1,11 +1,17 @@
 ﻿namespace HW_8_9
 {
     // Trying out Primary Constructor
-    public abstract class Shape(string name)
+    public abstract class Shape(string name): IComparable<Shape>
     {
         private readonly string name = name;
         public string Name { get { return name; } }
         public abstract double Area();
         public abstract double Perimeter();
+
+        public int CompareTo(Shape? other)
+        {   
+            if (other == null) return 1;
+            return Area().CompareTo(other.Area());
+        }
     }
 }
