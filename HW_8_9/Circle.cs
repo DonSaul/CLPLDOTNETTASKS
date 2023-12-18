2f9d79c58659478e859f7f94e54a5cbf1737e269
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-18 11:25:19 -0300

    Created Cirlce class

---
+++ HW_8_9/Circle.cs
@@ -0,0 +1,15 @@
+﻿namespace HW_8_9
+{
+    class Circle(string name, int radius) : Shape(name)
+    {
+        private readonly int radius = radius;
+        public override double Area()
+        {
+            return radius * radius * Math.PI;
+        }
+        public override double Perimeter()
+        {
+            return 2 * radius * Math.PI;
+        }
+    }
+}
