df50c2d7982f5b739ce21e73a6ade14df021c415
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-18 15:45:36 -0300

    Completed task a of H8

---
+++ HW_8_9/Homeworks.cs
@@ -0,0 +1,57 @@
+﻿using HW_8_9;
+
+class Homeworks
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--------Homework 8:--------");
+        ShowHomework8();
+        Console.WriteLine("--------Homework 9:--------");
+    }
+
+    private static void ShowHomework8()
+    {
+        List<Shape> shapes = [];
+        Console.WriteLine("Ok, it's your turn to give the raduises and sides for 5 circles and 5 squares:");
+        Console.WriteLine("If it's negative (why would you?), we'll ignore it and take the absolute value");
+        for (int i = 0; i < 5; i++)
+        {
+            bool rez;
+            double radius;
+            do
+            {
+                Console.Write("Circle {0} > ", i + 1);
+                string? s = Console.ReadLine();
+                rez = Double.TryParse(s, out radius);
+                if (!rez)
+                {
+                    Console.WriteLine("Not a number! Try again.");
+                }
+            } while (!rez);
+            shapes.Add(new Circle("Circle " + i.ToString(), Math.Abs(radius)));
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            bool rez;
+            double side;
+            do
+            {
+                Console.Write("Square {0} > ", i + 1);
+                string? s = Console.ReadLine();
+                rez = Double.TryParse(s, out side);
+                if (!rez)
+                {
+                    Console.WriteLine("Not a number! Try again.");
+                }
+            } while (!rez);
+            shapes.Add(new Circle("Square " + i.ToString(), Math.Abs(side)));
+        }
+
+        Console.WriteLine("Great! Here are the shapes we've just created:");
+        Console.WriteLine("Shape:         Area (u^2):    Perimeter:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("{0,-15}{1,-15:F3}{2:F3}", shape.Name, shape.Area(), shape.Perimeter());
+        }
+    }
+}
