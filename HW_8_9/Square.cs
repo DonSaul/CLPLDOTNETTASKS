814aa3326d32d7b0cc05a3f1b26d4e6a26827477
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-18 11:39:53 -0300

    Created Square class

---
+++ HW_8_9/Square.cs
@@ -0,0 +1,15 @@
+﻿namespace HW_8_9
+{
+    class Square(string name, double side) : Shape(name)
+    {
+        private double side = side;
+        public override double Area()
+        {
+            return side * side;
+        }
+        public override double Perimeter()
+        {
+            return side * 4;
+        }
+    }
+}
