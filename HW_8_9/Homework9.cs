f04f0aa25648d0a17fd07366be27fd285ddb57bc
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-19 17:39:32 -0300

    made Task A method static

--- HW_8_9/Homework9.cs
+++ HW_8_9/Homework9.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Net;
 internal class Homework9
 {
-    public void TaskA()
+    public static void TaskA()
     {
         Console.WriteLine("Creating list of shapes...");
         List<Shape> shapes = [
