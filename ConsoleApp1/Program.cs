24cf76677f01488a6755a2661c328ad7507d7805
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-12-20 17:52:13 -0300

    Refactored H1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,50 +3,61 @@ class Program
 {
     public static int Main(string[] args)
     {
+        Console.WriteLine("a) Get a square's perimeter and area from its side");
+        GetSquareMeasures();
+        Console.WriteLine("\nb) Ask the user (you) for a name and an age");
+        GetNameAndAge();
+        Console.WriteLine("\nc) Get the perimeter, area and volume, given a circle's radius");
+        GetCircleMeasuresFromRaduis();
+        return 0;
+    }
+
+    private static void GetSquareMeasures()
+    {
         Console.WriteLine("Insert a number for the square's side:");
-        string s = Console.ReadLine();
+        string? s = Console.ReadLine();
         int a;
         bool rez = int.TryParse(s, out a);
-        if (rez )
-        {
-            Console.WriteLine("Square's Perimeter: {0}", a * 4);
-            Console.WriteLine("Square's Area: {0}", a * a);
-        }
-        else
+        if (!rez)
         {
             Console.WriteLine("Whatever you just typed was not a number, sorry");
+            return;
         }
+        Console.WriteLine("Square's Perimeter: {0}", a * 4);
+        Console.WriteLine("Square's Area: {0}", a * a);
 
+    }
 
+    private static void GetNameAndAge()
+    {;
         string name;
         int age;
         Console.WriteLine("What is your name?");
-        name = Console.ReadLine();
+        name = Console.ReadLine() ?? "";
         Console.WriteLine("How old are you, {0}?", name);
-        s = Console.ReadLine();
-        rez = int.TryParse(s, out age);
-        if (rez)
-        {
-            Console.WriteLine("So you are {0}, aged {1} years. Nice to meet you!", name, age);
-        }
-        else
+        string? s = Console.ReadLine();
+        bool rez = int.TryParse(s, out age);
+        if (!rez)
         {
             Console.WriteLine("Whatever you just typed was not a number, sorry");
+            return;
         }
+        Console.WriteLine("So you are {0}, aged {1} years. Nice to meet you!", name, age);
+    }
+
+    private static void GetCircleMeasuresFromRaduis()
+    {
         Console.WriteLine("Input a valid circle raduis:");
         double r;
-        s = Console.ReadLine();
-        rez = double.TryParse(s, out r);
-        if (rez)
-        {
-            Console.WriteLine("Circunference length: {0:F3}", 2 * Math.PI * r);
-            Console.WriteLine("Circle area: {0:F3}", Math.PI * r * r);
-            Console.WriteLine("Sphere volume: {0:F3}", Math.PI * r * r * r * 4/3);
-        }
-        else
+        string? s = Console.ReadLine();
+        bool rez = double.TryParse(s, out r);
+        if (!rez)
         {
             Console.WriteLine("Whatever you just typed was not a number, sorry");
+            return;
         }
-        return 0;
+        Console.WriteLine("Circunference length: {0:F3}", 2 * Math.PI * r);
+        Console.WriteLine("Circle area: {0:F3}", Math.PI * r * r);
+        Console.WriteLine("Sphere volume: {0:F3}", Math.PI * r * r * r * 4/3);
     }
 }
