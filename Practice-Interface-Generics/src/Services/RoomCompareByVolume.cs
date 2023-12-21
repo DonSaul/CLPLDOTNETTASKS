2a7cdaf9d9ead7b533bd915bbf344e6f50316e1e
rainclaudio <rainclaudio25@gmai.com>
2023-12-21 10:43:18 -0300

    deleted Practice Interfaces Folder

--- Practice-Interface-Generics/src/Services/RoomCompareByVolume.cs
+++
@@ -1,7 +0,0 @@
-﻿public class RoomComparerByVolume<T> : IComparer<Room<T>> where T : IShape
-{
-    public int Compare(Room<T> x, Room<T> y)
-    {
-        return x.Volume().CompareTo(y.Volume());
-    }
-}
