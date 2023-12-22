bd7cd9f3b053a657cb69af03046d94f1273b7fdb
claudio <rainclaudio25@gmail.com>
2023-12-22 13:28:22 -0300

    small fix

--- Homework-4/H4-partB/H4-partB/Program.cs
+++ Homework-4/H4-partB/H4-partB/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions; 
 
 class Program
 {
@@ -11,16 +10,19 @@ class Program
         try
         {
             string[] lines = File.ReadAllLines(filePath);
-
-            string longestLine = "";
-            string shortestLine = lines.Length > 0 ? lines[0] : "";
             int maxLineLength = 50; // Maximum length for displayed lines
 
+            // Using LINQ to find the longest and shortest lines
+            var longestLine = lines.OrderByDescending(line => line.Length).FirstOrDefault() ?? "";
+            var shortestLine = lines.OrderBy(line => line.Length).FirstOrDefault() ?? "";
+
+            // Display Header
             Console.WriteLine("Line Analysis:");
             Console.WriteLine(new string('-', 60));
             Console.WriteLine($"{"Line",-50} | {"Symbols",7}");
             Console.WriteLine(new string('-', 60));
 
+            // Display each line and its length
             foreach (string line in lines)
             {
                 string truncatedLine = line.Length > maxLineLength
@@ -28,41 +30,33 @@ class Program
                                         : line;
 
                 Console.WriteLine($"{truncatedLine,-50} | {line.Length,7}");
-
-                if (line.Length > longestLine.Length) longestLine = line;
-                if (line.Length < shortestLine.Length) shortestLine = line;
             }
 
-            Console.WriteLine();
-            Console.WriteLine("Longest Line:");
+            // Display Longest and Shortest Lines
+            Console.WriteLine("\nLongest Line:");
             Console.WriteLine(longestLine.Length > maxLineLength ? longestLine.Substring(0, maxLineLength) + "..." : longestLine);
-            Console.WriteLine();
 
-            Console.WriteLine("Shortest Line:");
+            Console.WriteLine("\nShortest Line:");
             Console.WriteLine(shortestLine);
-            Console.WriteLine();
 
-            Console.WriteLine("Lines consisting of the word 'var':");
-            bool found = false; // Flag to track if any line is found
+            // Using LINQ to find lines consisting of 'var'
+            var varLines = lines.Where(line => line.Trim() == "var").ToList();
 
-            foreach (string line in lines)
+            Console.WriteLine("\nLines consisting of the word 'var':");
+            if (varLines.Any())
             {
-                if (line.Trim() == "var") // Check if the line is exactly 'var'
+                foreach (var line in varLines)
                 {
                     string truncatedLine = line.Length > maxLineLength
-                                                ? line.Substring(0, maxLineLength - 3) + "..."
-                                                : line;
+                                            ? line.Substring(0, maxLineLength - 3) + "..."
+                                            : line;
                     Console.WriteLine($"- {truncatedLine}");
-                    found = true; // Set flag to true as a line is found
                 }
             }
-
-            if (!found) // Check if no line was found
+            else
             {
                 Console.WriteLine("No lines consist only of the word 'var'.");
             }
-
-
         }
         catch (Exception ex)
         {
