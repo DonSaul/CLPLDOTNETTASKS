9244d4ec0af9a26dc8a4b549450e0e82ab1ee6e1
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2024-01-09 12:23:29 -0300

    Completed part 1 of Task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string output = $"{Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)}/rez.txt";
+string input = $"{Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)}/data.txt";
+
+Console.WriteLine($"Writing data from {input} to {output}...");
+
+using (StreamReader reader = new(input))
+{
+    using StreamWriter writer = new(output);
+    writer.Write(reader.ReadToEnd());
+}
+Console.WriteLine("Done.");
+
