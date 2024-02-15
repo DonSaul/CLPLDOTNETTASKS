4cfa1673d3a9858455355a1a7918304ea439289c
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2024-02-15 15:02:13 -0300

    fixed conditional display

--- react-hw/src/components/namedecor.js
+++ react-hw/src/components/namedecor.js
@@ -5,7 +5,7 @@ export const NameDecor = (props) => {
     const [isVisible, setIsVisible] = useState(false)
     return (
         <>
-        <button onClick={() => setIsVisible(!isVisible)}>{isVisible ? "Show me" : "Hide from me"} this name!</button>
+        <button onClick={() => setIsVisible(!isVisible)}>{!isVisible ? "Show me" : "Hide from me"} this name!</button>
         <p>{isVisible ? props.nombre : "Name is hidden!"}</p>{/* HW Task 3 */}
         </>
     )
