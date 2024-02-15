9bd3cb4606a46e4f6e849524f685e745fb839559
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2024-02-15 15:06:50 -0300

    Added components and namelist to app

--- react-hw/src/App.js
+++ react-hw/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { AlertForm } from './components/alertform';
+import { NameList } from './components/namelist';
 
 function App() {
+  const nameList = [
+    'Chita MacGuinness',
+    'Zlata Ahlström',
+    'Solvej Trueman',
+    'Mebh Cavanaugh',
+    'Edgars Bellerose',
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Example</h1>{/*HW Task 1*/}
       </header>
       <div className='content'>
-        
+        <NameList list={nameList}/>{/*HW Tasks 2-4*/}
+        <AlertForm/>{/*HW Task 5*/}
       </div>
     </div>
   );
